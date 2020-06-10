05472ce0b56f0733efde209c89c62f9c2cd8518f
Josh Maas-Howard <j.maas.howard@gmail.com>
2020-06-10 15:26:24 -0400

    Updates

---
+++ precache-manifest.a13c33c21dd18c090dd35a752ad2122a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c1ee28d65d7ea0c7b7afae9ba5baf40",
+    "url": "/amr-cra/index.html"
+  },
+  {
+    "revision": "28884bf38097f85093f0",
+    "url": "/amr-cra/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "f9c7508e9e4e5d49aedf",
+    "url": "/amr-cra/static/css/main.c3f65fe8.chunk.css"
+  },
+  {
+    "revision": "28884bf38097f85093f0",
+    "url": "/amr-cra/static/js/2.78baee5e.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/amr-cra/static/js/2.78baee5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9c7508e9e4e5d49aedf",
+    "url": "/amr-cra/static/js/main.4b52507a.chunk.js"
+  },
+  {
+    "revision": "d7895946fd6db3846ae9",
+    "url": "/amr-cra/static/js/runtime-main.4cbe4b32.js"
+  }
+]);
